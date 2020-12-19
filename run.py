cbf53b183cd6c43e3e2fcf4b5776c9f365ee3ddf
juneren26 <juneren26@gmail.com>
2020-12-19 11:08:17 +0800

    FS_WebUI基于关键字驱动结合Excel数据驱动自动化测试框架

---
+++ run.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+'''
+@Time   :2020/12/17 17:16
+@Author :六月
+@Email  :juneren26@gmail.com
+@File   :run.py
+@IDE    :PyCharm
+'''
+from Driver_execute.rundemo import readxls
+
+if __name__ == '__main__':
+    readxls('Testcase/FS_login.xlsx')
