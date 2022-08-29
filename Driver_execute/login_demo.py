aab62916111fccf9b96563a3dad69b8b6e82dfa9
juneren26 <70616792+juneren26@users.noreply.github.com>
2022-08-29 10:55:43 +0800

    Update login_demo.py

--- Driver_execute/login_demo.py
+++ Driver_execute/login_demo.py
@@ -18,7 +18,7 @@ from FSkeys.keywords import Webkeys
 # # 登陆账号
 # driver.get('https://tx.fs.com/cn/index.php?main_page=login')
 # driver.find_element_by_id('email_address_login').send_keys('scot.ren@feisu.com')
-# driver.find_element_by_id('password_login').send_keys('renzhao95626')
+# driver.find_element_by_id('password_login').send_keys('ren123456')
 # driver.find_element_by_id('user_login').click()
 # # 鼠标悬停
 # el = driver.find_element_by_link_text('我的账户')
@@ -34,7 +34,7 @@ from FSkeys.keywords import Webkeys
 driver = Webkeys('Chrome')
 driver.open('https://tx.fs.com/cn/index.php?main_page=login')
 driver.input('id','email_address_login','scot.ren@feisu.com')
-driver.input('id','password_login','renzhao95626')
+driver.input('id','password_login','ren123456')
 driver.click('id','user_login')
 driver.wait(3)
 driver.suspend('xpath','/html/body/div[1]/div/div[5]/div/a')
